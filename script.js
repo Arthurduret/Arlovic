28bb06e316ec2177749ef58b74f14169dea187c0
DURET Arthur <arthur.duret@arlovic.fr>
2026-01-18 21:00:46 +0100

    Header statique et dynamique fonctionnel

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ document.querySelectorAll('.m-hover').forEach(element => {
         if (link) window.location.href = link;
     });
 });
+
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('.header-panoramique');
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
+
+window.addEventListener('scroll', function() {
+    const stickyNav = document.getElementById('stickyNav');
+    
+    // Si on dépasse 200px de scroll, on affiche le menu
+    if (window.scrollY > 200) {
+        stickyNav.classList.add('visible');
+    } else {
+        stickyNav.classList.remove('visible');
+    }
+});
